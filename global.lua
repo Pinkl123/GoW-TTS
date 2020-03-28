5deca7cddcec0a7fadb938d74ca37e6a0094a9f1
gretmn102 <erlgreywatson@gmail.com>
2020-03-28 23:11:36 +0200

    import turn system

--- global.lua
+++ global.lua
@@ -1,33 +1,547 @@
-local seen={}
+-- Bundled by luabundle {"version":"1.5.0"}
+local __bundle_require, __bundle_loaded, __bundle_register, __bundle_modules = (function(superRequire)
+  local loadingPlaceholder = {[{}] = true}
 
-function dump(t,i)
-    seen[t]=true
-    local s={}
-    local n=0
-    for k in pairs(t) do
-        n=n+1 s[n]=k
+  local register
+  local modules = {}
+
+  local require
+  local loaded = {}
+
+  register = function(name, body)
+    if not modules[name] then
+      modules[name] = body
     end
-    table.sort(s)
-    for k,v in ipairs(s) do
-        print(i,v)
-        v=t[v]
-        if type(v)=="table" and not seen[v] then
-            dump(v,i.." ") -- и вновь вернем бесполезный комментарий
+  end
+
+  require = function(name)
+    local loadedModule = loaded[name]
+
+    if loadedModule then
+      if loadedModule == loadingPlaceholder then
+        return nil
+      end
+    else
+      if not modules[name] then
+        if not superRequire then
+          local identifier = type(name) == "string" and '"' .. name .. '"' or tostring(name)
+          error("Tried to require " .. identifier .. ", but no such module has been registered")
+        else
+          return superRequire(name)
         end
+      end
+
+      loaded[name] = loadingPlaceholder
+      loadedModule = modules[name](require, loaded, register, modules)
+      loaded[name] = loadedModule
     end
-end
 
---[[ Lua code See documentation: https://api.tabletopsimulator.com/ --]]
+    return loadedModule
+  end
 
---[[ The osnLoad event is called after the game save finishes loading. --]]
-function onLoad()
-    -- print('onLoad!')
-end
+  return require, loaded, register, modules
+end)(nil)
+__bundle_register(
+  "__root",
+  function(require, _LOADED, __bundle_register, __bundle_modules)
+    --[[
+      shell mod to test and share turnButton code
+    ]]
+    require("Lua/tools")
 
---[[ The onUpdate event is called once per frame. --]]
-function onUpdate()
-    --[[ print('onUpdate loop!') --]]
-    -- print(getGlobalScriptVar("stats").MPC)
-end
+    --[[ The onUpdate event is called once per frame. --]]
+    -- function onUpdate()
+    --   --[[ print('onUpdate loop!') --]]
+    --   -- print(getGlobalScriptVar("stats").MPC)
+    -- end
+    function onSave()
+      local stat = {
+        playing = playing,
+        tBSave = tB.save()
+      }
+      return JSON.encode(stat)
+    end
 
--- dump(_G, "")
+    function onLoad(statstr)
+      math.randomseed(os.time()) -- non-repeatable randoms
+      game_status = JSON.decode(statstr)
+      if game_status then
+        playing = game_status.playing
+        tB.restore(game_status.tBSave)
+      else
+        playing = false
+      end
+      if playing then
+        tB.show({color = tB.curPlayer, text = "End " .. tB.curPlayer .. "'s turn"})
+      else
+        print("Not playing yet")
+        local stuff = getAllObjects()
+        for i, o in ipairs(stuff) do
+          if o.tag == "Generic" then
+            -- if there is a noises, turnButton will use it.  most of my mods
+            -- gather this data about everything in the "room".
+            if o.getName() == "Noises" then
+              tB.noises = o.getGUID()
+            -- tB.button = tB.button or o.getGUID()
+            end
+          end
+          if o.tag == "Tile" then
+            if o.name == "turnButton" then
+              tB.button = o.getGUID()
+            end
+          end
+        end
+        tB.init("Black")
+      end
+    end -- of onLoad
+
+    function onChat(what, who)
+      if what == "\\q" then
+        endGame()
+        return false
+      elseif what == "\\p" then -- force end of turn
+        tB.pass()
+        return false
+      elseif what == "\\t" then -- hide/show global turnButton display
+        tB.toggleGlobalPanel()
+        return false
+      elseif what == "\\r" then
+        tB.offset = {x = 220, y = 0}
+        tB.restore(tB.save())
+        tB.show(tB.curPlayer, "End " .. tB.curPlayer .. "'s turn")
+      end
+    end -- of onChat
+
+    function endGame()
+      print("game aborted")
+      playing = false
+      tB.init()
+      onLoad(onSave())
+    end
+
+    function sayTo(who, what) -- lazy; not used in turnButton per se.
+      broadcastToAll(what, who)
+    end
+
+    require("Lua/turnButton") -- require adds a ".ttslua"
+    --------------------------------------------------------------------------------
+    -- these are called from the turn system if then exist
+    --------------------------------------------------------------------------------
+    function initGame()
+      -- your mod specific code here.
+      playing = true
+    end -- of initGame
+
+    --------------------------------------------------------------------------------
+    function onTurnStart()
+      -- your mod specific turn start actions here
+      sayTo(tB.curPlayer, tB.curPlayer .. ", it is your turn.")
+      tB.playNoise(3) -- a rooster sound in my bundle.
+    end
+
+    function onTurnEnd()
+      -- your mod specific turn end actions here
+      sayTo(tB.curPlayer, tB.curPlayer .. ", your turn is ended.")
+    end
+  end
+)
+__bundle_register(
+  "Lua/turnButton",
+  function(require, _LOADED, __bundle_register, __bundle_modules)
+    -------------------------------------------------------------
+    -- TurnButton declarations
+    -------------------------------------------------------------
+
+    -- UI work around declarations -- no post processing of UI data so have to use top level names
+    function tB_start(player, value, id)
+      tB.start(player, value, id)
+    end
+
+    function tB_endTurn(player, value, id)
+      tB.endTurn(player, value, id)
+    end
+
+    function tB_beginMoveButton(player, value, id)
+      tB.beginMoveButton(player, value, id)
+    end
+
+    function tB_endMoveUI(player, value, id)
+      tB.endMoveButton(player, value, id)
+    end
+
+    function tB_toggleGlobalPanel(player, value, id)
+      tB.toggleGlobalPanel(player, value, id)
+    end
+
+    ------------------------------------------------------------------------
+    -- required by turnButton, but might be duplicate
+
+    function deepCopy(original)
+      if type(original) ~= "table" then
+        return original
+      else
+        local copy = {}
+        for i, v in pairs(original) do
+          copy[i] = deepCopy(v)
+        end
+        return copy
+      end
+    end -- of deepCopy
+
+    function doNothing() -- UI click function REQUIRED by turnButton
+    end -- of doNothing
+    -------------------------------------------------------
+    -- everything else lives inside tB
+    -------------------------------------------------------
+    tB = {
+      UITable = {}, -- Global UI
+      UIIndex = 1,
+      UITrigger = false, -- make true to update Global UI with UITable
+      triggerIndex = nil,
+      curPlayer = "Black",
+      curIndex = 0,
+      order = {},
+      button = "b7757c", -- The noises object is as good as any to attact the turnButton
+      globalPanel = true,
+      offset = {x = -220, y = 0}, -- initial location of global panel relative to upper right screen corner
+      noises = "b7757c", -- assetBundle with some sounds -- overridden if another generic assetrbundle is named Noises -- we use index 1 and 3.
+      -- top level UI for turnButton
+      -- manage details of the on-screen turnButton here.
+      tbLayout = {
+        tag = "Panel",
+        attributes = {
+          id = "turnButton",
+          width = "150",
+          height = "25",
+          color = "#444555",
+          rectAlignment = "UpperRight",
+          offsetXY = "-250 0",
+          allowDragging = "true",
+          returnToOriginalPositionWhenReleased = "false",
+          onEndDrag = "tB_endMoveButton"
+        },
+        children = {
+          {
+            tag = "Button",
+            attributes = {
+              id = "closeButton",
+              width = "20",
+              height = "20",
+              rectAlignment = "UpperRight",
+              color = "#990000",
+              textColor = "#FFFFFF",
+              text = "X",
+              onClick = "tB_toggleGlobalPanel"
+            }
+          },
+          {
+            tag = "Text",
+            attributes = {
+              text = "Turn Button",
+              alignment = "UpperLeft",
+              fontSize = "18",
+              offsetXY = "5 0",
+              fontStyle = "Bold",
+              color = "#FFFFFF"
+            }
+          },
+          {
+            tag = "GridLayout",
+            attributes = {
+              id = "orderList",
+              columns = "1",
+              width = "100%",
+              cellsize = "150 25",
+              rectAlignment = "TopCenter",
+              offsetXY = "0 -20"
+            },
+            children = {}
+          }
+        }
+      },
+      -- top level UI for button; need to change to classical UI -- new UI interferes with TTS operations (UI eats clicks)
+      -- Edit this table to match your specific button; this is set up to work with the noise cube I am using.
+      buttonTable = {
+        width = 4500,
+        height = 800,
+        position = {0, 0.5, 0},
+        color = {0.6, 0.4, 0.3},
+        hover_color = {0.5, 0.5, 0.5},
+        press_color = {1, 1, 1},
+        label = "Start the Game",
+        font_size = 600,
+        font_color = {0, 0, 0},
+        click_function = "tB_start",
+        function_owner = Global
+      },
+      -- manage the look and feel of your player buttons here
+      globalXmlTable = {
+        tag = "Button",
+        attributes = {
+          id = "playerButton",
+          width = "150",
+          height = "25",
+          text = "Start the Game",
+          fontSize = "20",
+          textColor = "Black",
+          colors = "#cccccc|#dddddd|#444444|#00000000",
+          onClick = "tB_start"
+        }
+      },
+      -------------------------------------------------------
+      -- set up the buttons with "start the game".
+      init = function()
+        tB.UITable = UI.getXmlTable()
+        tB.UIIndex = #tB.UITable + 1
+        for i, item in ipairs(tB.UITable) do
+          if item.attributes.id == "turnButton" then
+            tB.UIIndex = i
+            break -- shouldn't ever be more than one.
+          end
+        end
+        -- kick off maintenance function to update once a second
+        tB.triggerIndex = Wait.time(tB.updateUI, 1, -1)
+        tB.show({color = "Black", text = "Start the game"})
+      end, -- of initTurns
+      updateUI = function()
+        if tB then
+          if tB.UITrigger then
+            UI.setXmlTable(tB.UITable)
+            tB.UITrigger = false
+          end
+        end
+      end,
+      -------------------------------------------------------
+      -- configure turnButton, then call initGame
+      -- currently initialize3s order to getSeatedPlayers()
+      start = function(p1, p2, p3) -- either obj, color, clicktype or player, value, id and value is nil in this app
+        local player = p1 -- screen UI click
+        log(p1)
+        log(p2)
+        log(p3)
+        if p3 ~= "playerButton" then -- button classic UI click
+          player = Player[p2]
+        end
+        broadcastToAll("Game started by " .. player.color, player.color)
+        tB.order =
+          table.sort(
+          getSeatedPlayers(),
+          function(a, b)
+            return Player[a].getHandTransform().rotation.y < Player[b].getHandTransform().rotation.y
+          end
+        )
+        tB.tbLayout.attributes.height = tostring(25 * #tB.order + 25)
+        tB.curIndex = math.random(#tB.order)
+        tB.curPlayer = tB.order[tB.curIndex]
+        tB.globalXmlTable.attributes.onClick = "tB_endTurn"
+        broadcastToAll("Game started by " .. player.color, player.color)
+        broadcastToAll(tB.curPlayer .. ", you are first.", tB.curPlayer)
+        tB.show({color = tB.curPlayer, text = "End " .. tB.curPlayer .. "'s turn"})
+        tB.playNoise(1)
+        if initGame then
+          initGame(player)
+        end
+      end, -- end of startTurns
+      ----------------------------------------------------------------
+      -- display turnButtons
+      -- parameters are:
+      --  color = current player color in order, or "Black"
+      --  text = string to display in current player's button
+      --
+      --
+      show = function(params) -- display turnButtons
+        -- print("tB.show called", " ", params.color, " ", params.text )
+        tB.buttonTable.font_color = params.color
+        tB.buttonTable.label = params.text
+        tB.buttonTable.click_function = "tB_endTurn"
+        if params.color == "Black" then
+          tB.buttonTable.click_function = "tB_start"
+          tB.globalXmlTable.attributes.onClick = "tB_start"
+          tB.globalXmlTable.attributes.textColor = "Black"
+          tB.globalXmlTable.attributes.text = params.text
+          tB.tbLayout.children[3].children = tB.globalXmlTable
+        else
+          tB.tbLayout.children[3].children = {}
+          for _, player in ipairs(tB.order) do
+            tB.globalXmlTable.attributes.textColor = player
+            if player == params.color then
+              tB.globalXmlTable.attributes.onClick = "tB_endTurn"
+              tB.globalXmlTable.attributes.text = params.text
+            else
+              tB.globalXmlTable.attributes.onClick = "doNothing"
+              tB.globalXmlTable.attributes.text = player
+            end
+            table.insert(tB.tbLayout.children[3].children, deepCopy(tB.globalXmlTable))
+          end
+        end
+
+        if tB.globalPanel then
+          -- log(string.format("offset: %8.2f %8.2f", tB.offset.x, tB.offset.y))
+          -- log(tB.UIIndex)
+          tB.UITable[tB.UIIndex] = deepCopy(tB.tbLayout)
+        end
+        tB.UITrigger = true
+        local button = getObjectFromGUID(tB.button)
+        button.clearButtons()
+        button.createButton(tB.buttonTable)
+      end, -- of show
+      -- Return entry in order just after current player
+      next = function()
+        local ix = tB.curIndex + 1
+        if ix > #tB.order then
+          ix = 1
+        end
+        return tB.order[ix]
+      end,
+      -- Return entry in order just before current player
+      prev = function()
+        local ix = tB.curIndex - 1
+        if ix < 1 then
+          ix = #tB.order
+        end
+        return tB.order[ix]
+      end, -- of prev
+      -- force end of current player's turn bypassing rule that only current player may end hes or her turn.
+      pass = function()
+        tB.endTurn(nil)
+      end, -- of passTurn
+      -- Make it newPlayer's turn -- newPlayer is a color
+      set = function(newPlayer)
+        local p = tB.curPlayer
+        tB.curPlayer = newPlayer
+        for i, color in ipairs(tB.order) do
+          if color == tB.curPlayer then
+            broadcastToAll(tB.curPlayer .. ", it is your turn.", tB.curPlayer)
+            tB.curIndex = i
+            tB.show({color = tB.curPlayer, text = "End " .. tB.curPlayer .. "'s turn"})
+            return
+          end
+        end
+        print(tB.curPlayer, " is not in the player list (order)")
+        tB.curPlayer = p
+      end, -- of setTurn
+      -- player clicked end turn
+      endTurn = function(p1, p2, p3)
+        --  log(p1) log(p2) log(p3)
+        local color = p2
+        if p3 == "playerButton" then -- from global UI
+          color = color.color
+        end
+        if color and color ~= tB.curPlayer then
+          broadcastToAll(
+            (color or "nil") .. ", You cannot end another player's turn (" .. (tB.curPlayer or "nil") .. ")",
+            {0.8, 0, 0}
+          )
+          return
+        end
+        if onTurnEnd then
+          onTurnEnd()
+        end -- call user turn end function
+        tB.curIndex = tB.curIndex + 1
+        if tB.curIndex > #tB.order then
+          tB.curIndex = 1
+        end
+        tB.curPlayer = tB.order[tB.curIndex]
+        tB.show({color = tB.curPlayer, text = "End " .. tB.curPlayer .. "'s turn"})
+        if onTurnStart then
+          onTurnStart()
+        end -- call user turn start function
+      end, -- of endTurn
+      -- Show or hide the global UI
+      toggleGlobalPanel = function()
+        tB.globalPanel = not tB.globalPanel
+        if tB.globalPanel then
+          Global.UI.show("turnButton")
+        else
+          Global.UI.hide("turnButton")
+        end
+        return tB.globalPanel
+      end, -- of toggleGlobalPanel
+      -----------------------------------------------------------------------
+      -- implements onSave and onLoad support
+      save = function()
+        local tBData = {
+          order = tB.order,
+          curIndex = tB.curIndex,
+          curPlayer = tB.curPlayer,
+          button = tB.noises, -- custom button later if we want
+          globalPanel = tB.globalPanel,
+          UITable = tB.UITable,
+          UIIndex = tB.UIIndex,
+          offset = tB.offset,
+          noises = tB.noises
+        }
+        return tBData
+      end, -- of save
+      restore = function(tBData)
+        if tBData then
+          for k, v in pairs(tBData) do
+            tB[k] = v
+          end
+        end
+        if tB.triggerIndex then
+          Wait.stop(tB.triggerIndex)
+        end
+        tB.triggerIndex = Wait.time(tB.updateUI, 1, -1)
+      end,
+      -------------------------------------------------------
+      -- Utility functions
+      -------------------------------------------------------
+      playNoise = function(index) -- play a sound if we can.
+        if tB.noises then
+          local obj = getObjectFromGUID(tB.noises)
+          obj.AssetBundle.playTriggerEffect(index)
+        end
+      end, -- of playNoise
+      -------------------------------------------------------
+      -- These functions are experimental and tempramental.
+      -- To position your turnButton dynamically, you need to...
+      -- 1.  use topDown camera
+      -- 2.  set view directly over center of table
+      -- 3. zoom untill table just fills screen vertically
+      -- 4. adjust x and y calculations.  Only constants should be changed.
+      -- 5 TEST and repeat :-)
+      -------------------------------------------------------
+
+      -------------------------------------------------------
+      -- complete drag response
+      endMoveUI = function(player, value, id)
+        local newPos = player.getPointerPosition() -- assumes topDown view on 26x15 viewport
+        log(string.format("endDrag: %8.2f %8.2f", newPos.x, newPos.z))
+        local x = math.max(math.min((newPos.x - 26) * 35, 0), -1760)
+        local y = math.max(math.min((newPos.z - 16) * 35, 0), -1024)
+        log(string.format("%6.2f %6.2f -> %6.2f %6.2f", tB.offset.x, tB.offset.y, x, y))
+        tB.offset = {x = x, y = y}
+        tB.tbLayout.attributes.offsetXY = string.format("%d %d", tB.offset.x, tB.offset.y)
+      end -- of endMoveTurnButton
+    }
+
+    --------------------------------------------------------------------------------
+    -- END of turnButton code
+    --------------------------------------------------------------------------------
+  end
+)
+__bundle_register(
+  "Lua/tools",
+  function(require, _LOADED, __bundle_register, __bundle_modules)
+    local seen = {}
+    function dump(t, i)
+      seen[t] = true
+      local s = {}
+      local n = 0
+      for k in pairs(t) do
+        n = n + 1
+        s[n] = k
+      end
+      table.sort(s)
+      for k, v in ipairs(s) do
+        print(i, v)
+        v = t[v]
+        if type(v) == "table" and not seen[v] then
+          dump(v, i .. " ") -- и вновь вернем бесполезный комментарий
+        end
+      end
+    end
+  end
+)
+return __bundle_require("__root")
